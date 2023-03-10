6c8e4cd6454151ccb9a19744c6688da776ba122d
Armatys22 <124389724+Armatys22@users.noreply.github.com>
2023-03-10 17:27:30 +0000

    made it work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Dominik Armatys
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  document.getElementById("multiply-math").innerHTML = "<p>The area is: " + (5 * 3) + "cm²</p>"
+  document.getElementById("subtract-math").innerHTML = "<p>The perimeter is:  " + (7 - 3) + "cm</p>"  
+}
